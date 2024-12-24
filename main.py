a25bf59195e3bf741394b538542171ad8f7085d9
methamphetaminelab <58376312+methamphetaminelab@users.noreply.github.com>
2024-12-24 17:39:28 +0300

    init

--- main.py
+++ main.py
@@ -28,10 +28,16 @@ async def installDriver():
         options = webdriver.ChromeOptions()
         prefs = {"download.default_directory": downloadFolder}
         options.add_experimental_option("prefs", prefs)
-        options.add_argument("--headless")
 
+        # Отключаем headless режим, чтобы можно было видеть браузер.
+        # Для обычного использования можно оставить это закомментированным.
+        options.add_argument("--headless") 
+
+        options.add_argument("--disable-gpu")  # Некоторые сайты могут блокировать headless браузеры по этой причине
+        options.add_argument("--no-sandbox")
         driver = webdriver.Chrome(options=options)
-        logger.info("ChromeDriver installed")
+
+        logger.success("ChromeDriver installed")
 
         logger.info("Opening browser")
         driver.get("https://osu.ppy.sh/community/chat?channel_id=32359962")
@@ -112,7 +118,6 @@ async def downloadMap(link):
         driver.switch_to.window(driver.window_handles[-1])
         
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='btn-osu-big btn-osu-big--beatmapset-header ']"))).click()
-        logger.success(f"Map downloaded: {link}")
         
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
@@ -129,21 +134,18 @@ async def installMaps():
 
         for map in maps:
             mapPath = os.path.join(downloadFolder, map)
-            
-            if mapPath.endswith(".tmp"):
-                continue
 
-            if mapPath.endswith(".crdownload"):
+            if mapPath.endswith(".crdownload") or mapPath.endswith(".tmp"):
                 logger.info(f"Waiting for map: {map}")
 
-                while mapPath.endswith(".crdownload"):
+                while mapPath.endswith(".crdownload") or mapPath.endswith(".tmp"):
                     
                     await asyncio.sleep(0.1)
 
                     maps = os.listdir(downloadFolder)
                     for map in maps:
                         mapPath = os.path.join(downloadFolder, map)
-                        if mapPath.endswith(".crdownload"):
+                        if mapPath.endswith(".crdownload") or mapPath.endswith(".tmp"):
                             break
                     else:
                         break
@@ -164,6 +166,7 @@ async def main():
     for _ in range(count):
         link = await sendMessage()
         download_task = asyncio.create_task(downloadMap(link))
+        await asyncio.sleep(1)
         install_task = asyncio.create_task(installMaps())
 
         await download_task
